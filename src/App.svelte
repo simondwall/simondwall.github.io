9c3b5d8f42671e32d356ea67eea22baa50ab746c
simondawall <simondanielwall@gmail.com>
2022-12-22 15:56:48 +0100

    Create basic layout for page

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,81 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+    /* import Background from "./Background.svelte"; */
+    import Header from "./Header.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<div id=background>
+    <!--
+    <Background />
+    -->
+</div>
 
-  <div class="card">
-    <Counter />
-  </div>
+<div class=scroll-container>
+    <div class=full-screen-div>
+        <Header/>
+    </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+    <div class=full-screen-div>
+        <h1 class=section>General</h1>
+    </div>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+    <div class=full-screen-div>
+        <h1 class=section>Education</h1>
+    </div>
+
+    <div class=full-screen-div>
+        <h1 class=section>Career</h1>
+    </div>
+
+    <div class=full-screen-div>
+        <h1 class=section>Skills</h1>
+    </div>
+
+    <div class=full-screen-div>
+        <h1 class=section>Projects</h1>
+    </div>
+
+    <div class=full-screen-div>
+        <h1 class=section>Setup Preference</h1>
+    </div>
+
+    <div class=full-screen-div>
+        <h1 class=section>Hobbies</h1>
+    </div>
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    @import url('https://fonts.googleapis.com/css?family=JetBrains+Mono');
+
+    :global(body) {
+        margin: 0;
+    }
+
+    .section {
+        font-family: 'JetBrains Mono', monospace;
+        text-align: center;
+        margin: 0;
+    }
+
+    .scroll-container {
+        position: relative;
+        scroll-snap-type: y mandatory;
+        height: 100vh;
+        overflow-y: scroll;
+    }
+
+    .full-screen-div {
+        width: 100vw;
+        height: 100vh;
+        margin: 0;
+
+        scroll-snap-align: start;
+        /* only supported in Chrome */
+        scroll-snap-stop: always;
+    }
+
+    #background {
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+    }
 </style>
---
+++ src/Header.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+</script>
+
+<div id=container-container>
+    <div class=container>
+        <span id=s>S</span>
+        <span id=i>i</span>
+        <span id=m>m</span>
+        <span id=o>o</span>
+        <span id=n>n</span>
+    </div>
+    <div class=container>
+        <span id=d>D</span>
+        <span id=dot>.</span>
+        <span id=space>&nbsp</span>
+        <span id=w>W</span>
+        <span id=a>a</span>
+        <span id=l-1>l</span>
+        <span id=l-2>l</span>
+    </div>
+</div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=JetBrains+Mono');
+
+    #container-container {
+        margin: 0;
+        width: 100vw;
+        height: 100vh;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .container {
+        display: flex;
+        /* flex-direction: column; */
+        align-items: center;
+        justify-content: center;
+
+        font-family: 'JetBrains Mono', monospace;
+
+        text-shadow: 10px 20px 30px #afafaf;
+    }
+
+    #s, #i, #m, #o, #n {
+        font-size: 20vw;
+    }
+
+    #d, #dot, #space, #w, #a, #l-1, #l-2 {
+        font-size: 10vw;
+    }
+
+    @keyframes slide-in-from-top {
+        from {
+            transform: translateY(-30vh);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0vh);
+            opacity: 100;
+        }
+    }
+
+    @keyframes slide-in-from-bottom {
+        from {
+            transform: translateY(30vh);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0vh);
+            opacity: 100;
+        }
+    }
+
+    #s {
+        animation-name: slide-in-from-top;
+        animation-duration: 2s;
+    }
+
+    #i {
+        animation-name: slide-in-from-top;
+        animation-duration: 2.7s;
+    }
+
+    #m {
+        animation-name: slide-in-from-top;
+        animation-duration: 3.6s;
+    }
+
+    #o {
+        animation-name: slide-in-from-top;
+        animation-duration: 1.1s;
+    }
+
+    #n {
+        animation-name: slide-in-from-top;
+        animation-duration: 2.5s;
+    }
+
+    #d {
+        animation-name: slide-in-from-bottom;
+        animation-duration: 4.7s;
+    }
+
+    #dot {
+        animation-name: slide-in-from-bottom;
+        animation-duration: 5.2s;
+    }
+
+    #w {
+        animation-name: slide-in-from-bottom;
+        animation-duration: 5.1s;
+    }
+
+    #a {
+        animation-name: slide-in-from-bottom;
+        animation-duration: 4.2s;
+    }
+
+    #l-1 {
+        animation-name: slide-in-from-bottom;
+        animation-duration: 3.9s;
+    }
+
+    #l-2 {
+        animation-name: slide-in-from-bottom;
+        animation-duration: 3.3s;
+    }
+</style>
